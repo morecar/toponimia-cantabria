3fb4ca59f447778661edecdd86eaaee74f2d10e3
mmoreno <mmoreno@newrelic.com>
2021-02-25 02:25:36 +0100

    chore: add hash version control & extract gsheets

--- src/model/pointRepositoryFactory.js
+++ src/model/pointRepositoryFactory.js
@@ -1,27 +1,32 @@
 import PointRepository from './pointRepository'
 
-export async function  buildRepositoryFrom(googleSheet) {
-    return shouldReload(googleSheet) 
-        ? reloadLocalDatabase(googleSheet).then(localDatabase => new PointRepository(JSON.parse(localDatabase)))
-        : Promise.resolve(new PointRepository(JSON.parse(localStorage.getItem("localIndex"))))
+
+export async function buildRepositoryFromSheet(spreadsheet) {
+    if (shouldReload(spreadsheet.hash)) reloadLocalDatabase(spreadsheet.hash, spreadsheet.sheet)
+  
+    return buildRepositoryFromLocal()
 }
 
-async function reloadLocalDatabase(googleSheet) {
-    const remoteElementCount = googleSheet.rowCount
+
+async function buildRepositoryFromLocal() {
+    return Promise.resolve(new PointRepository(JSON.parse(localStorage.getItem("localIndex"))))
+}
+
+async function reloadLocalDatabase(newHash, googleSheet) {
     googleSheet.getRows().then(rows => {
         const remoteDatabase = rows.map(row => ({ title: row.name, type: row.type, coordinates: row.coordinates.trim().split(';').map(pair => pair.trim().split(',').map(parseFloat)), tags: row.tags.split(',')}))
         localStorage.setItem("localIndex", JSON.stringify(remoteDatabase) )
-        localStorage.setItem("localIndexSize", remoteElementCount)
+        localStorage.setItem("localIndexHash", newHash)
+
+        console.log(`Database: updated to ${newHash}`)
     })
-    return localStorage.getItem("localIndex")
 }
 
-function shouldReload(googleSheet) {
-    const localStorageElementCount = parseInt(localStorage.getItem("localIndexSize"))
-    const remoteDbElementCount = parseInt(googleSheet.rowCount)
+function shouldReload(newHash) {
+    const currentHash = localStorage.getItem("localIndexHash")
 
-    const reload = localStorageElementCount !== remoteDbElementCount
+    const reload = newHash !== currentHash
 
-    console.log(`Database: reload=${reload}`)
+    console.log(`Database: current is ${currentHash}, reload=${reload}`)
     return reload
 }
--- src/staticData/googleCredentials.js
+++ src/staticData/googleCredentials.js
@@ -1,5 +1,7 @@
 export const GOOGLE_SHEETS_API_KEY = 'AIzaSyBl_GJjcKmg4Lk0dWPLzGX6bXpT0vXW5R0'
 
 export const GOOGLE_DOC_ID = '1Rd8TQOp2hSBOxhSeAaLeb_faICF2_oAv2-jtn2vgXrY'
-export const GOOGLE_SHEET_ID = '0'
+export const GOOGLE_DB_SHEET_TITLE = 'DB'
+export const GOOGLE_INFO_SHEET_TITLE = 'INFO'
+export const GOOGLE_INFO_SHEET_CELL = 'A1'
 
