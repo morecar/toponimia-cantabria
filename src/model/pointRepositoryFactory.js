fb2825dbc1395bda1155b9177aa1fb5532d41698
mmoreno <mmoreno@newrelic.com>
2021-02-25 01:10:55 +0100

    bugfix: page needs reloading to display data

--- src/model/pointRepositoryFactory.js
+++ src/model/pointRepositoryFactory.js
@@ -2,17 +2,18 @@ import PointRepository from './pointRepository'
 
 export async function  buildRepositoryFrom(googleSheet) {
     return shouldReload(googleSheet) 
-        ? reloadLocalDatabase(googleSheet).then(localDatabase => new PointRepository(localDatabase))
+        ? reloadLocalDatabase(googleSheet).then(localDatabase => new PointRepository(JSON.parse(localDatabase)))
         : Promise.resolve(new PointRepository(JSON.parse(localStorage.getItem("localIndex"))))
 }
 
 async function reloadLocalDatabase(googleSheet) {
     const remoteElementCount = googleSheet.rowCount
-    return googleSheet.getRows().then(rows => {
+    googleSheet.getRows().then(rows => {
         const remoteDatabase = rows.map(row => ({ title: row.name, type: row.type, coordinates: row.coordinates.trim().split(';').map(pair => pair.trim().split(',').map(parseFloat)), tags: row.tags.split(',')}))
         localStorage.setItem("localIndex", JSON.stringify(remoteDatabase) )
         localStorage.setItem("localIndexSize", remoteElementCount)
     })
+    return localStorage.getItem("localIndex")
 }
 
 function shouldReload(googleSheet) {
