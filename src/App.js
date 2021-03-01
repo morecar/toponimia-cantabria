51bd081a03986a7847c118885ad03b10fc0b9c40
mmoreno <mmoreno@newrelic.com>
2021-03-01 02:47:04 +0100

    bugfix: fix routing

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ import ResultsRoute from './components/routing/ResultsRoute'
 import CaptureRoute from './components/routing/CaptureRoute'
 import HomeRoute from './components/routing/HomeRoute'
 
-import {ROUTE_HOME, ROUTE_CAPTURE, ROUTE_SEARCH, ROUTE_RESULT} from './staticData/localization.js'
+import { ROUTE_HOME, ROUTE_CAPTURE, ROUTE_SEARCH, ROUTE_RESULT } from './staticData/localization.js'
 
 export default function App(props) {
     return (
-        <Router>
+        <Router basename={"/toponimia-cantabria"}>
             <Switch>
                 <Route path={ROUTE_CAPTURE} children={<CaptureRoute {...props} />} />
                 <Route path={ROUTE_SEARCH} children={<ResultsRoute {...props} />} />
